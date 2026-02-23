9f761bbf5d6592fcad793793c776d7ec986e10fb
Bryam R Norabuena <bryam.norabuena@pe.ey.com>
2026-02-23 09:44:36 -0500

    Addition mockups folder

---
+++ mockups/token.py
@@ -0,0 +1,37 @@
+mockup_data = {
+  '001': {
+    'user': 'gianina.a.ramirez@pe.ey.com', 'contract': 2, 'customer': 'UPL-2021-00010', 'customerName': 'UPL COLOMBIA', 'fathersName': 'Ruiz', 'firstName': 'Luis Alberto', 'mothersName': 'Canales', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_SYSADMIN', 'GLOCONTROVERSY_CUSTOMER'], 'status': 'Success'
+  },
+  '002': {
+    'user': 'alexander.a.romero@pe.ey.com', 'contract': '1', 'customer': 'MAN9209143V1', 'customerName': 'MANCERA', 'fathersName': 'Rojas', 'firstName': 'Alex Manuel', 'mothersName': 'Borda', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'taxID': 'JIJJ931217MX2', 'language': 'en_MX', 'roles': ['GLOCONTROVERSY_SYSADMIN', 'GLOCONTROVERSY_PROJADMIN'], 'status': 'Success'},
+  '003': {
+    'user': 'controv.admin@pe.ey.com', 'contract': '1', 'customer': 'MAN9209143V1', 'customerName': 'MANCERA', 'fathersName': 'Quiroz', 'firstName': 'Julio Cesar', 'mothersName': 'Ramos', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_PROJADMIN'], 'status': 'Success'},
+  '004': {
+    'user': 'controv.editor@pe.ey.com', 'contract': 3, 'customer': 'LG20809143V2', 'customerName': 'LG Electronics', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9g', 'language': 'es_MX', 'roles': ['EMP_PORTAL_USER', 'EMP_PORTAL_ADMIN'], 'status': 'Success'},
+  '006': {
+    'user': 'user02@portal.ey.com', 'contract': '20', 'customer': 'DXX10209569V1', 'customerName': 'DXN E.I.R.L.', 'fathersName': 'Rivera', 'firstName': 'Mario', 'mothersName': 'Cusi', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_PROJEDIT'], 'status': 'Success'},
+  '007': {
+    'user': 'alexander.a.romero@pe.ey.com', 'contract': 'TEST001', 'customer': 'MAN9209143V1', 'customerName': 'MANCERA', 'fathersName': 'Rios', 'firstName': 'Jose Luis', 'mothersName': None, 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'taxID': 'JIJJ931217MX2', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_PROJADMIN'], 'status': 'Success'},
+  '008': {
+    'user': 'controv.admin.tester@pe.ey.com', 'contract': '2', 'customer': 'MAN9209566V2', 'customerName': 'MANCERA v2', 'fathersName': 'Sanchez', 'firstName': 'Luis Manuel', 'mothersName': 'Leon', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'taxID': 'XIXX931217MX3', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_PROJADMIN'], 'status': 'Success'},
+  '009': {
+    'user': 'gianina.a.ramirez@pe.ey.com',
+    'contract': 'TEST001',
+    'customer': 'MAN9209143V1',
+    'customerName': 'MANCERA',
+    'fathersName': 'Chavez',
+    'firstName': 'Luis Ricardo',
+    'mothersName': 'Aliaga',
+    'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f',
+    'taxID': 'JIJJ931217MX2',
+    'language': 'es_MX',
+    'roles': ['GLOCONTROVERSY_SYSADMIN'],
+    'status': 'Success'
+  },
+  '010': {
+    'user': 'estefania.jimenez.gutierrez@co.ey.com', 'contract': 'CNT9004625', 'customer': '900462511-9', 'customerName': 'BRITISH AMERICAN TOBACCO COLOMBIA S A S', 'fathersName': 'Jimenez', 'firstName': 'Estefania', 'mothersName': 'Gutierrez', 'token': '458b0771-b65a-45af-98aa-8e8c9a415a9f', 'taxID': 'JIJJ931217MX2', 'language': 'es_MX', 'roles': ['GLOCONTROVERSY_PROJADMIN'], 'status': 'Success'
+    },
+  'Error': {
+    'status': 'Error', 'message': 'Invalid'
+  }
+}
